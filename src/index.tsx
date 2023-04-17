3598bb2214891a3b6a3819017ff7ed2921fe8614
FantasyGmm <16450052+FantasyGmm@users.noreply.github.com>
2023-04-17 21:48:53 +0800

    add a simpe auth

--- src/index.tsx
+++ src/index.tsx
@@ -11,12 +11,61 @@ if (!rootElement) throw new Error('Missing root');
 
 const root = createRoot(rootElement);
 
-root.render(
-	<StrictMode>
-		<HelmetProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</HelmetProvider>
-	</StrictMode>
-);
+if (window.localStorage.getItem("isLogin") !== "0") {
+  let account = "";
+  const setAccount = (e: string) => {
+    account = e;
+  };
+  let password = "";
+  const setPassword = (e: string) => {
+    password = e;
+  };
+
+  const saveToken = () => {
+    console.log("save");
+    window.localStorage.setItem("isLogin", "0");
+    window.location.href = "/";
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    // simpe auth
+    if (account === "test" && password === "test") {
+      saveToken();
+    } else {
+      alert("Invalid account or password!");
+    }
+    e.preventDefault();
+  };
+  root.render(
+    <>
+      <main>
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <p>Account:</p>
+              <input type="text" onChange={e => setAccount(e.target.value)} />
+            </label>
+            <label>
+              <p>Password:</p>
+              <input type="password" onChange={e => setPassword(e.target.value)} />
+            </label>
+            <p>
+              <button type="submit">Submit</button>
+            </p>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+} else {
+  root.render(
+    <StrictMode>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </StrictMode>
+  );
+}
